315ef6facc741ca6c7a3d2a1c0a28bc379e18e7e
Amir Tuval <amirtu@tabnine.com>
2023-07-03 22:57:07 +0300

    pyton setup

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.dap = {
+  plugin = true,
   n = {
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
@@ -13,6 +14,37 @@ M.dap = {
         sidebar.open()
       end,
       "Open debugging sidebar"
+    },
+    ["<F5>"] = {
+      "<cmd> DapContinue <CR>",
+      "Start debugging or continue if already in debugging"
+    },
+    ["<leader><F5>"] = {
+      "<cmd> DapTerminate <CR>",
+      "Stop Debugging",
+    },
+    ["<F8>"] = {
+      "<cmd> DapStepOver <CR>",
+      "Step Over",
+    },
+    ["<F7>"] = {
+      "<cmd> DapStepInto <CR>",
+      "Step Into",
+    },
+    ["<leader><F7>"] = {
+      "<cmd> DapStepOut <CR>",
+      "Step Out",
+    }
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
     }
   }
 }
