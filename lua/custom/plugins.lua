f6d6e446ee3b3d67f1d1a4edad6c520d8c1f9775
Amir Tuval <amirtu@tabnine.com>
2023-07-05 12:43:35 +0300

    attempt 1 - node debugging - still not working
    Disable cmp tab navigation to allow tabnine sleection

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -147,6 +147,47 @@ local plugins = {
       require('core.utils').load_mappings('neogit')
     end
   },
+  {
+     "microsoft/vscode-js-debug",
+     build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+  },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+    ft = {"javascript", "typescript"},
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      require("dap-vscode-js").setup({
+        -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+        debugger_path = "~/.local/share/nvim/lazy/vscode-js-debug", -- Path to vscode-js-debug installation.
+        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+        -- log_file_path = "~/logs/ts-debug.log", -- Path for file logging
+        -- log_file_level = vim.log.levels.DEBUG, -- Logging level for output to file. Set to false to disable file logging.
+        log_console_level = vim.log.levels.DEBUG -- Logging level for output to console. Set to false to disable console output.
+      })
+
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+          {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require'dap.utils'.pick_process,
+              cwd = "${workspaceFolder}",
+            }
+          }
+        }
+      end
+    end
+  }
 }
 
 return plugins
